1184733ae1def1ac175a9b222d3b56a67723fbfd
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-01-21 17:50:57 -0600

    Added SussyBakaRunner files, added main.cpp work to interact with the new files.

---
+++ src/SussyBakaRunner.cpp
@@ -0,0 +1,21 @@
+// Trevor Dohm
+// Gabriel Mongaras
+// Algorithms
+#include "SussyBakaRunner.h"
+
+void SussyBakaRunner::loadData(char* inputFile) {
+    l = loadData(inputFile);
+
+}
+
+void SussyBakaRunner::runBruteForce() {
+
+}
+
+void SussyBakaRunner::runHighValue() {
+
+}
+
+void SussyBakaRunner::runCustom() {
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+// Trevor Dohm
+// Gabriel Mongaras
+// Algorithms
 #include <iostream>
+#include "SussyBakaRunner.h"
 
-
-
-int main() {
-    std::cout << "Hello World" << std::endl;
+int main(int argc, char* argv[]) {
+    SussyBakaRunner SBR;
+    SBR.loadData(argv[1]);
+    SBR.runBruteForce();
+    SBR.runHighValue();
+    SBR.runCustom();
+    return 0;
 }
