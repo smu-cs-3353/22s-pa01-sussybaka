46aa1fdece25340380bb7b5d85a48ebac24996c7
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-01-21 18:06:55 -0600

    Merged with main, added back object running.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 // Algorithms
 #include <iostream>
 #include "Loader.h"
+#include "SussyBakaRunner.h"
 
-
-
-int main(int argc, char** argv) {
+// Hewwo! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Dis is our new pwoject! We worked
+// weally hawd on it and i hope you enjoy! ★~(◡﹏◕✿)
+int main(int argc, char* argv[]) {
     // If argc is less than 2, stop the program
     if (argc < 2) {
         std::cout << "Program must have at least one argument" << std::endl;
@@ -17,6 +18,12 @@ int main(int argc, char** argv) {
     Loader l;
     std::string temp(argv[1]);
     l.loadData(temp);
+
+    SussyBakaRunner SBR;
+    SBR.loadData(argv[1]);
+    SBR.runBruteForce();
+    SBR.runHighValue();
+    SBR.runCustom();
 
     std::cout << std::endl;
     return 0;
