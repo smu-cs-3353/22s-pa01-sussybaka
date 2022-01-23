a8afde7c066468764d5771e537970ed1747794e9
gmongaras <gabriel@mongaras.com>
2022-01-23 14:51:54 -0600

    Added Progress Messages When Running Program

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,32 @@
 int main(int argc, char* argv[]) {
     // If argc is less than 2, stop the program
     if (argc < 2) {
-        std::cout << "Program must have at least one argument" << std::endl;
+        std::cout << "Program must have at least one argument. Usage:" << std::endl;
+        std::cout << "./22s-pa01-sussybaka [Input File Name]" << std::endl;
         return 1;
     }
 
-    // Create a loader object using the given filename
-    Loader l;
-    std::string temp(argv[1]);
-    l.loadData(temp);
-
     SussyBakaRunner SBR;
+
+    // Load the data
+    std::cout << "Loading In Data..." << std::endl;
     SBR.loadData(argv[1]);
+    std::cout << "Data Loaded!" << std::endl << std::endl;
+
+    // Run the brute force algorithm
+    std::cout << "Running the Brute Force algorithm..." << std::endl;
     SBR.runBruteForce();
+    std::cout << "Brute Force Finished!" << std::endl << std::endl;
+
+    // Run the high value algorithm
+    std::cout << "Running the High Value algorithm..." << std::endl;
     SBR.runHighValue();
+    std::cout << "High Value Finished!" << std::endl << std::endl;
+
+    // Run the custom algorithm
+    std::cout << "Running the Custom algorithm..." << std::endl;
     SBR.runCustom();
+    std::cout << "Custom Finished!" << std::endl;
 
     return 0;
 }
