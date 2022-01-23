d8261c3d799f97c5f9b27ee7f4aa87eade54c3c8
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-01-23 16:18:28 -0600

    Code Quality Corrections to Main

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,12 @@
 // Gabriel Mongaras
 // Algorithms
 #include <iostream>
-#include "Loader.h"
 #include "SussyBakaRunner.h"
-
-
 
 // Hewwo! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Dis is our new pwoject! We worked
 // weally hawd on it and i hope you enjoy! ★~(◡﹏◕✿)
 int main(int argc, char* argv[]) {
+
     // If argc is less than 2, stop the program
     if (argc < 2) {
         std::cout << "Program must have at least one argument. Usage:" << std::endl;
@@ -40,4 +38,5 @@ int main(int argc, char* argv[]) {
     std::cout << "Custom Finished!" << std::endl;
 
     return 0;
+
 }
