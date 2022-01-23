40104f106a1b628a7102f6d77dbd916225a428e5
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-01-23 12:32:49 -0600

    Created Branch, pushing HighValue.

---
+++ src/HighValue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LFL on 1/23/2022.
+//
+
+#include "HighValue.h"
---
+++ src/HighValue.h
@@ -0,0 +1,68 @@
+#ifndef INC_22S_PA01_SUSSYBAKA_HIGHVALUE_H
+#define INC_22S_PA01_SUSSYBAKA_HIGHVALUE_H
+
+#include <vector>
+#include "Picture.h"
+#include "Loader.h"
+#include <bits/stdc++.h>
+
+class HighValue {
+private:
+    // Max values
+    int maxWidth;
+    int maxHeight;
+    int maxImages;
+
+    // Vector to hold all possible combinations
+    std::vector<std::vector<Picture>> combinations;
+
+    // Combination of pictures with the highest price
+    std::vector<Picture> bestCombo;
+    float bestPrice;
+
+
+
+
+
+    /**
+     * findCombos Method
+     * Recursive function to find all possible combinations the data can form.
+     * @param index Current index being iterated over in the Loader data vector
+     * @param buffer Buffer to hold the current combination when iterating
+     *               over all possible combinations
+     * @param bufferPrice The price of the current combination in the buffer
+     * @param curWidth The current width of the picture buffer
+     * @param data A Loader object with the loaded data
+     */
+    void findCombos(int index, std::vector<Picture>& buffer, float& bufferPrice, int& curWidth, Loader& data);
+
+    /**
+     * createFile
+     * Creates the output file for the best combination found
+     * @param inFileName The input file name that will be adjusted for the output file
+     */
+    void createFile(std::string& inFileName);
+
+
+public:
+    /**
+     * Constructors
+     */
+    BruteForce();
+    BruteForce(Loader& data);
+
+
+    /**
+     * runBruteForce Method
+     * Find the best possible combination using brute force
+     * @param data A Loader object with the loaded data
+     */
+    void runBruteForce(Loader& data);
+};
+
+
+
+};
+
+
+#endif //INC_22S_PA01_SUSSYBAKA_HIGHVALUE_H
