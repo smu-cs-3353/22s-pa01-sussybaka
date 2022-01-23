3497d603f808555f7966913fd56dd6682e7ba3e7
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-01-23 16:28:26 -0600

    Code Quality Corrections to SBR.h

--- src/SussyBakaRunner.h
+++ src/SussyBakaRunner.h
@@ -3,11 +3,14 @@
 // Algorithms
 #ifndef INC_22S_PA01_SUSSYBAKA_SUSSYBAKARUNNER_H
 #define INC_22S_PA01_SUSSYBAKA_SUSSYBAKARUNNER_H
+
 #include "Loader.h"
 #include "BruteForce.h"
 #include "HighValue.h"
 #include "Custom.h"
 
+// Class responsible for running
+// all of the necessary methods.
 class SussyBakaRunner {
 
 private:
