5f2f5063c0524ee52dc1bcb641cf28f05935dca7
gmongaras <gabriel@mongaras.com>
2022-01-28 16:51:22 -0600

    Added Timing Data and Updated README

--- src/main-timed.cpp
+++ src/main-timed.cpp
@@ -3,6 +3,7 @@
 // Algoworithms
 #include <iostream>
 #include "SussyBakaRunner.h"
+#include <chrono>
 
 // Hewwo! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Dis is our new pwoject! We worked
 // weally hawd on it and i hope you enjoy! ★~(◡﹏◕✿)
@@ -28,9 +29,13 @@ int main(int argc, char* argv[]) {
     }
     else {
         if (std::string(argv[2]) == std::string("-b")) {
+            auto start = std::chrono::high_resolution_clock::now();
             std::cout << "Running the Brute Force algorithm..." << std::endl;
             SBR.runBruteForce();
-            std::cout << "Brute Force Finished!" << std::endl << std::endl;
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+            std::cout << "Brute Force Finished!" << std::endl;
+            std::cout << "Time to finish Brute Force: " << ((float)duration.count())/1000 << " microseconds" << std::endl << std::endl;
         }
         else {
             std::cout << "Not Running The Brute Force Algorithm" << std::endl << std::endl;
@@ -39,13 +44,21 @@ int main(int argc, char* argv[]) {
 
     // Run the high value algorithm
     std::cout << "Running the High Value algorithm..." << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
     SBR.runHighValue();
-    std::cout << "High Value Finished!" << std::endl << std::endl;
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "High Value Finished!" << std::endl;
+    std::cout << "Time to finish High Value: " << ((float)duration.count())/1000 << " microseconds" << std::endl << std::endl;
 
     // Run the custom algorithm
     std::cout << "Running the Custom algorithm..." << std::endl;
+    start = std::chrono::high_resolution_clock::now();
     SBR.runCustom();
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << "Custom Finished!" << std::endl;
+    std::cout << "Time to finish Custom: " << ((float)duration.count())/1000 << " microseconds" << std::endl;
 
     return 0;
 
